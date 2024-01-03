77d238b40396e316dd1f378abceada975b8c02da
cccaternberg <acaternberg@cloudbees.com>
2024-01-03 18:20:27 +0100

    add

---
+++ copy-jobs-to-seperateNamespaceByEBSSnapshot.sh
@@ -0,0 +1,127 @@
+#! /bin/bash
+set -x
+DOMAIN_SOURCE=${1:-"ebs"}
+DOMAIN_DESTINATION=${2:-"sepns"}
+NAMESPACE_SOURCE=${3:-"cloudbees-core"}
+NAMESPACE_DESTINATION=${4:-"cloudbees-controllers"}
+export GENDIR=generated
+
+mkdir -p $GENDIR
+export AWS_DEFAULT_REGION=us-east-1
+
+TAGS="Tags=[{Key=cb-environment,Value=customer-dev-worldpay},{Key=cb-user,Value=acaternberg},{Key=cb-owner,Value=professional-services}]"
+
+#The JENKINS_HOME PV name where we want to take a snapshot from
+VOLUMENAME=$(kubectl get "pvc/jenkins-home-${DOMAIN_SOURCE}-0" -n ${NAMESPACE_SOURCE} -o go-template={{.spec.volumeName}})
+#The volume id of the PV
+VOLUMEID=$(kubectl get pv $VOLUMENAME -n ${NAMESPACE_SOURCE} -o go-template={{.spec.awsElasticBlockStore.volumeID}})
+
+echo "take snapshot for $DOMAIN_SOURCE, $VOLUMENAME, $VOLUMEID"
+SNAPSHOT=$(aws ec2 create-snapshot \
+--volume-id "$VOLUMEID" \
+--description "$DOMAIN_SOURCE,$VOLUMENAME,$VOLUMEID" \
+--output json \
+--tag-specifications "ResourceType=snapshot,$TAGS")
+echo $SNAPSHOT |jq  >  $GENDIR/ebs-snapshot.json
+
+SNAPSHOTID=$(cat $GENDIR/ebs-snapshot.json |jq -r '.SnapshotId')
+aws ec2 wait snapshot-completed \
+    --snapshot-ids $SNAPSHOTID
+echo "snapshot $SNAPSHOTID created"
+
+echo "create volume for $SNAPSHOTID"
+SNAPSHOT_VOLUME=$(aws ec2 create-volume \
+--volume-type gp2 \
+--snapshot-id $SNAPSHOTID \
+--tag-specifications "ResourceType=volume,$TAGS" \
+--availability-zone us-east-1a \
+--output json)
+echo $SNAPSHOT_VOLUME |jq  >  $GENDIR/ebs-snapshot_volume.json
+export VOLUME_ID=$(cat $GENDIR/ebs-snapshot_volume.json |jq -r  '.VolumeId')
+echo "volume $VOLUME_ID created"
+
+
+#create PV,PVC fro the new volume (restored from EBS Snapshot)
+cat <<EOF | kubectl --namespace=$NAMESPACE_DESTINATION apply -f -
+---
+apiVersion: "v1"
+kind: PersistentVolume
+metadata:
+  name: rescue-pv-${DOMAIN_SOURCE}
+spec:
+  capacity:
+    storage: 50Gi  # Define your desired storage size
+  volumeMode: Filesystem
+  accessModes:
+    - ReadWriteOnce  # Replace with appropriate access mode (e.g., ReadWriteOnce)
+  persistentVolumeReclaimPolicy: Delete  # Adjust reclaim policy based on your requirement
+  storageClassName: gp2
+  awsElasticBlockStore:
+    volumeID: "${VOLUME_ID}"  # Replace with your AWS EBS volume ID
+    fsType: ext4  # Define the file system type
+    readOnly: false  # Set to true if the volume should be mounted as read-only
+---
+apiVersion: "v1"
+kind: PersistentVolumeClaim
+metadata:
+  name: rescue-pvc-${DOMAIN_SOURCE}
+  namespace: ${NAMESPACE_DESTINATION}
+spec:
+  storageClassName: gp2
+  volumeName: rescue-pv-${DOMAIN_SOURCE}
+  accessModes:
+    - ReadWriteOnce  # Choose appropriate access mode based on your requirement
+  resources:
+    requests:
+      storage: 50Gi  # Set your desired storage size
+EOF
+
+
+#Create the rescue pod that mount the snapshot volume and the new controller volume
+cat <<EOF | kubectl --namespace=$NAMESPACE_DESTINATION  apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: rescue-pod
+spec:
+  securityContext:
+    runAsUser: 1000
+    runAsGroup: 1000
+    fsGroup: 1000
+  volumes:
+    - name: pv-data-source
+      persistentVolumeClaim:
+        claimName: rescue-pvc-${DOMAIN_SOURCE}
+    - name: pv-data-destination
+      persistentVolumeClaim:
+        claimName: jenkins-home-${DOMAIN_DESTINATION}-0
+   # emptyDir: {}
+  containers:
+  - name: rescue-pvc
+    #image: busybox
+    image: instrumentisto/rsync-ssh
+    command: [ "sh", "-c", "sleep 10000" ]
+    volumeMounts:
+    - name:  pv-data-source
+      mountPath: /tmp/jenkins_home_source
+    - name: pv-data-destination
+      mountPath: /tmp/jenkins_home_destination
+    securityContext:
+      allowPrivilegeEscalation: false
+EOF
+
+#Wait until pod is up
+kubectl wait pod/rescue-pod  --for condition=ready
+#This command will sync all jobs and jobs folders excluding the build
+kubectl exec -ti rescue-pod -- rsync -avz --exclude="*/builds/" /tmp/jenkins_home_source/jobs/ /tmp/jenkins_home_destination/jobs
+
+#Clean resources
+##TODO: use trap command and sigterm
+echo "delete snapshot $SNAPSHOTID, we don't need it anymore"
+aws ec2 delete-snapshot --snapshot-id $SNAPSHOTID
+echo "delete rescue-pod , we don't need it anymore"
+kubectl delete pod rescue-pod
+echo "delete  rescue-pvc-${DOMAIN_SOURCE} , we don't need it anymore"
+kubectl delete pvc rescue-pvc-${DOMAIN_SOURCE}
+echo "delete snapshot volume   ${VOLUME_ID} , we don't need it anymore"
+aws ec2 delete-volume --volume-id ${VOLUME_ID}
