15856299d7084fcb38a9cce9f7658ffecb1de36c
cccaternberg <acaternberg@cloudbees.com>
2024-01-07 15:29:14 +0100

    rename var GENDIR, add copy EFS

--- copy-jobs-to-seperateNamespaceByEFS.sh
+++ copy-jobs-to-seperateNamespaceByEFS.sh
@@ -1,25 +1,75 @@
 #! /bin/bash
+#enable debugging output
 set -x
 #Name of the original Team or Managed Controller you want to copy jobs from
-DOMAIN_SOURCE=${1:-"teams-myteam"}
+DOMAIN_SOURCE=${1:-"myteam"}
+
+#Teamcontrollers have always the prefix "teams-". If the source controller is a managed controller, set the DOMAIN_SOURCE_TEAM_PREFIX to empty string""
+#Team Controller prefix
+DOMAIN_SOURCE_TEAM_PREFIX="teams-"
+#Managed Controller prefix
+#DOMAIN_SOURCE_TEAM_PREFIX=""
+
 #Name of the destination Team or Managed Controller you want to copy jobs to
 DOMAIN_DESTINATION=${2:-"sepns-efs"}
+
 #Name of the original namespace where your $DOMAIN_SOURCE Controller is located
 NAMESPACE_SOURCE=${3:-"cloudbees-core"}
+
 #Name of the destination namespace where your $DOMAIN_DESTINATION Controller is located
 NAMESPACE_DESTINATION=${4:-"cloudbees-controllers"}
 
 #Adjust your AWS region
 export AWS_DEFAULT_REGION=us-east-1
+
+export BASE_URL="https://ci.acaternberg.pscbdemos.com"
+export CJOC_URL=${BASE_URL}"/cjoc"
+export TOKEN="admin123:11346c2a9e3d0b84c7f094bdcb2cdbf010"
+
+#Temporary dir for generated files and log files
 export GENDIR=generated
 mkdir -p $GENDIR
 
-#TODO:Adjust your tags here
-TAGS="Tags=[{Key=cb-environment,Value=customer-dev-XY},{Key=cb-user,Value=XY},{Key=cb-owner,Value=XY}]"
+#Helper function to check if an controller is online
+function checkControllerOnline () {
+  # We have to wait until ingress is created and we can call the Jenkins HealthCheck with state 200
+  while [ ! -n "$(curl  -IL  ${1}/whoAmI/api/json?tree=authenticated | grep -o  'HTTP/2 200')" ]
+  do
+    #echo "wait 30 sec for State HTTP 200:  ${CONTROLLER_URL}/login"
+    echo "wait 30 sec for State HTTP 200:  ${1}/whoAmI/api/json?tree=authenticated"
+    sleep 10
+  done
+}
+
+#CREATE MC CONTROLLER
+# We apply the cjoc-controller-items.yaml to cjoc. Cjoc will create a new Managed Controller for us using our $GENDIR/${CONTROLLER_NAME}.yaml
+echo "------------------  CREATING MANAGED CONTROLLER ------------------"
+#export CONTROLLER_NAME="${DOMAIN_DESTINATION}"
+#envsubst < templates/create-mm.yaml > $GENDIR/${DOMAIN_DESTINATION}.yaml
+#curl -XPOST \
+#   --user $TOKEN \
+#   "${CJOC_URL}/casc-items/create-items" \
+#    -H "Content-Type:text/yaml" \
+#   --data-binary @$GENDIR/${DOMAIN_DESTINATION}.yaml
+# We wait until our new Managed Controller pod is up
+#echo "------------------  WAITING FOR CONTROLLER TO COME UP ------------------"
+#checkControllerOnline $BASE_URL/$DOMAIN_DESTINATION
+
+
+# Now we apply the target Folder to the Managed Controller.
+# This is the root folder where we want to migrate our credentials and jobs to
+echo "------------------  CREATING INITIAL TEAM FOLDER ------------------"
+export CONTROLLER_NAME="${DOMAIN_SOURCE}"
+envsubst < templates/create-folder.yaml > ${GENDIR}/${DOMAIN_SOURCE}-folder.yaml
+curl -v  -XPOST \
+    --user $TOKEN \
+    "${BASE_URL}/${DOMAIN_DESTINATION}/casc-items/create-items" \
+     -H "Content-Type:text/yaml" \
+    --data-binary @${GENDIR}/${DOMAIN_SOURCE}-folder.yaml -o ${GENDIR}/${DOMAIN_SOURCE}-create-folder-output.log
 
 #Get the SOURCE JENKINS_HOME PV name where we want to take a snapshot from
 # We also get some attributes from from the original pv and pvc that we need to reclaim the EFS volume
-VOLUME_NAME_SOURCE=$(kubectl get "pvc/jenkins-home-${DOMAIN_SOURCE}-0" -n ${NAMESPACE_SOURCE} -o go-template={{.spec.volumeName}})
+VOLUME_NAME_SOURCE=$(kubectl get "pvc/jenkins-home-${DOMAIN_SOURCE_TEAM_PREFIX}${DOMAIN_SOURCE}-0" -n ${NAMESPACE_SOURCE} -o go-template={{.spec.volumeName}})
 VOLUME_HANDLE_SOURCE=$(kubectl get pv $VOLUME_NAME_SOURCE -n ${NAMESPACE_SOURCE} -o go-template={{.spec.csi.volumeHandle}})
 VOLUME_ATTRIBUTE_SOURCE=$(kubectl get pv $VOLUME_NAME_SOURCE -o   go-template={{.spec.csi.volumeAttributes}} | sed  's#map\[storage.kubernetes.io/csiProvisionerIdentity:# #'  | sed 's#]##')
 VOLUME_CAPACITY_STORAGE_SOURCE=$(kubectl get pv $VOLUME_NAME_SOURCE -o   go-template={{.spec.capacity.storage}})
@@ -94,25 +144,31 @@ spec:
 EOF
 
 #Wait until pod is up
-kubectl wait pod/rescue-pod  --for condition=ready
+kubectl wait pod/rescue-pod  --for condition=ready --timeout=60s
+
+echo "########SYNC JOBS########"
 #This command will sync all jobs and folders excluding the build
-kubectl exec -ti rescue-pod -- rsync -avz --exclude="*/builds/" /tmp/jenkins_home_source/jobs/ /tmp/jenkins_home_destination/jobs
+#time kubectl exec -ti rescue-pod -- rsync -avz --exclude="*/builds/" /tmp/jenkins_home_source/jobs/$DOMAIN_SOURCE/jobs/ /tmp/jenkins_home_destination/jobs/$DOMAIN_SOURCE/jobs
+time kubectl exec -ti rescue-pod -- rsync -az  /tmp/jenkins_home_source/jobs/$DOMAIN_SOURCE/jobs/ /tmp/jenkins_home_destination/jobs/$DOMAIN_SOURCE/jobs/
 
-#This command will sync all jobs and folders including history
-#kubectl exec -ti rescue-pod -- rsync -avz  /tmp/jenkins_home_source/jobs/ /tmp/jenkins_home_destination/jobs
+#cp seems to faster rather than rsync
+time kubectl exec -ti rescue-pod -- cp -Rf /tmp/jenkins_home_source/jobs/$DOMAIN_SOURCE/jobs /tmp/jenkins_home_destination/jobs/$DOMAIN_SOURCE/
 
+
+#reload new Jobs from disk
+curl -L -s -u $TOKEN -XPOST  "https://ci.acaternberg.pscbdemos.com/$DOMAIN_DESTINATION/reload" \
+
+echo "########CLEANUP RESOURCES########"
 #Clean resources
 function cleanUpResources {
   echo "delete rescue-pod , we don't need it anymore"
-  kubectl delete pod rescue-pod -n $NAMESPACE_DESTINATION
+  kubectl delete pod rescue-pod -n $NAMESPACE_DESTINATION || true
   echo "delete rescue-pvc-${DOMAIN_SOURCE} , we don't need it anymore"
-  kubectl delete pvc rescue-efs-pvc-${DOMAIN_SOURCE} -n $NAMESPACE_DESTINATION
-  kubectl delete pvc rescue-efs-pv-${DOMAIN_SOURCE}
+  kubectl delete pvc rescue-efs-pvc-${DOMAIN_SOURCE} -n $NAMESPACE_DESTINATION || true
+  kubectl delete pv rescue-efs-pv-${DOMAIN_SOURCE} || true
   kubectl get pod,pv,pvc -n $NAMESPACE_DESTINATION
 }
 #https://www.putorius.net/using-trap-to-exit-bash-scripts-cleanly.html#google_vignette
 trap cleanUpResources  SIGINT SIGTERM ERR EXIT
 
 
-#reload new Jobs from disk
-#TODO: restart Controller ${DOMAIN_DESTINATION} or reload configuration from disk
--- migrateTC2MC.sh
+++ migrateTC2MC.sh
@@ -6,13 +6,13 @@ source ./envvars.sh
 export CONTROLLER_NAME=${1:-$CONTROLLER_NAME}
 export CONTROLLER_URL=${BASE_URL}"/"${CONTROLLER_NAME}
 
-GEN_DIR=gen
-rm -rf $GEN_DIR
-mkdir -p $GEN_DIR
+GENDIR=gen
+rm -rf $GENDIR
+mkdir -p $GENDIR
 
 function checkControllerOnline () {
   # We have to wait until ingress is created and we can call the Jenkins HealthCheck with state 200
-  while [ ! -n "$(curl  -IL  ${CONTROLLER_URL}/login | grep -o  'HTTP/2 200')" ]
+  while [ ! -n "$(curl  -IL  ${CONTROLLER_URL}/whoAmI/api/json?tree=authenticated | grep -o  'HTTP/2 200')" ]
   do
     #echo "wait 30 sec for State HTTP 200:  ${CONTROLLER_URL}/login"
     echo "wait 30 sec for State HTTP 200:  ${CONTROLLER_URL}/whoAmI/api/json?tree=authenticated"
@@ -22,21 +22,21 @@ function checkControllerOnline () {
 
 # We render the CasC template instances for cjoc-controller-items.yaml and the casc-folder (target folder)
 # All variables from the envvars.sh will be substituted
-envsubst < ${CREATE_MM_TEMPLATE_YAML} > $GEN_DIR/${CONTROLLER_NAME}.yaml
-envsubst < ${CREATE_MM_FOLDER_TEMPLATE_YAML} > $GEN_DIR/${CONTROLLER_NAME}-folder.yaml
+envsubst < ${CREATE_MM_TEMPLATE_YAML} > $GENDIR/${CONTROLLER_NAME}.yaml
+envsubst < ${CREATE_MM_FOLDER_TEMPLATE_YAML} > $GENDIR/${CONTROLLER_NAME}-folder.yaml
 
 # We switch to the cloudbees namespace, where the TC runs
 #kubens $NAMESPACE
 kubectl config set-context $(kubectl config current-context) --namespace=$NAMESPACE_CB_CORE
 
 #CREATE MC CONTROLLER
-# We apply the cjoc-controller-items.yaml to cjoc. Cjoc will create a new MC for us using our $GEN_DIR/${CONTROLLER_NAME}.yaml
+# We apply the cjoc-controller-items.yaml to cjoc. Cjoc will create a new MC for us using our $GENDIR/${CONTROLLER_NAME}.yaml
 echo "------------------  CREATING MANAGED CONTROLLER ------------------"
 curl -XPOST \
    --user $TOKEN \
    "${CJOC_URL}/casc-items/create-items" \
     -H "Content-Type:text/yaml" \
-   --data-binary @$GEN_DIR/${CONTROLLER_NAME}.yaml
+   --data-binary @$GENDIR/${CONTROLLER_NAME}.yaml
 
 # We wait until our new Managed Controller pod is up
 echo "------------------  WAITING FOR CONTROLLER TO COME UP ------------------"
@@ -49,17 +49,17 @@ curl -v  -XPOST \
     --user $TOKEN \
     "${CONTROLLER_URL}/casc-items/create-items" \
      -H "Content-Type:text/yaml" \
-    --data-binary @$GEN_DIR/${CONTROLLER_NAME}-folder.yaml -o $GEN_DIR/create-folder-output.log
+    --data-binary @$GENDIR/${CONTROLLER_NAME}-folder.yaml -o $GENDIR/create-folder-output.log
 # curl  -XPOST -u ${TOKEN} ${CONTROLLER_URL}/casc-items/create-items -d @${CONTROLLER_NAME}-folder.yaml
 # sleep 180
 
 # COPY JOBS
 # We copy the jobs folder recursive from TC to the new folder on MC
 echo "------------------  COPYING JOBS FOLDER ------------------"
-mkdir -p $GEN_DIR/teams-${CONTROLLER_NAME}-jobs
+mkdir -p $GENDIR/teams-${CONTROLLER_NAME}-jobs
 #kubectl exec -it teams-${CONTROLLER_NAME}-0 --  tar -cvzf /tmp/${CONTROLLER_NAME}-job.tar.gz -C /var/jenkins_home/jobs/
-kubectl cp teams-${CONTROLLER_NAME}-0:var/jenkins_home/jobs/${CONTROLLER_NAME}/jobs/ $GEN_DIR/teams-${CONTROLLER_NAME}-jobs/
-kubectl cp $GEN_DIR/teams-${CONTROLLER_NAME}-jobs/. ${CONTROLLER_NAME}-0:var/jenkins_home/jobs/${CONTROLLER_NAME}/jobs
+kubectl cp teams-${CONTROLLER_NAME}-0:var/jenkins_home/jobs/${CONTROLLER_NAME}/jobs/ $GENDIR/teams-${CONTROLLER_NAME}-jobs/
+kubectl cp $GENDIR/teams-${CONTROLLER_NAME}-jobs/. ${CONTROLLER_NAME}-0:var/jenkins_home/jobs/${CONTROLLER_NAME}/jobs
 #kubectl exec -it ${CONTROLLER_NAME}-0 --  tar -xvf /tmp/${CONTROLLER_NAME}-job.tar.gz -C /var/jenkins_home/jobs/ 
 
 # Now we restart the MC
@@ -70,28 +70,28 @@ checkControllerOnline
 echo "------------------  EXPORT FOLDER CREDENTIALS  ------------------"
 curl -o ./credentials-migration/export-credentials-folder-level.groovy https://raw.githubusercontent.com/cloudbees/jenkins-scripts/master/credentials-migration/export-credentials-folder-level.groovy
 curl --data-urlencode "script=$(cat ./credentials-migration/export-credentials-folder-level.groovy)" \
---user $TOKEN ${BASE_URL}/teams-${CONTROLLER_NAME}/scriptText  -o $GEN_DIR/test-folder.creds
-tail -n 1  $GEN_DIR/test-folder.creds | sed  -e "s#\[\"##g"  -e "s#\"\]##g"  | tee  $GEN_DIR/folder-imports.txt
+--user $TOKEN ${BASE_URL}/teams-${CONTROLLER_NAME}/scriptText  -o $GENDIR/test-folder.creds
+tail -n 1  $GENDIR/test-folder.creds | sed  -e "s#\[\"##g"  -e "s#\"\]##g"  | tee  $GENDIR/folder-imports.txt
 
 # IMPORT FOLDER CREDENTIALS
 echo "------------------  IMPORT FOLDER CREDENTIALS  ------------------"
-kubectl cp $GEN_DIR/folder-imports.txt ${CONTROLLER_NAME}-0:/var/jenkins_home/
+kubectl cp $GENDIR/folder-imports.txt ${CONTROLLER_NAME}-0:/var/jenkins_home/
 curl -o ./credentials-migration/update-credentials-folder-level.groovy https://raw.githubusercontent.com/cloudbees/jenkins-scripts/master/credentials-migration/update-credentials-folder-level.groovy  
-cat ./credentials-migration/update-credentials-folder-level.groovy | sed  "s#^\/\/ encoded.*#encoded = [new File(\"/var\/jenkins_home\/folder-imports.txt\").text]#g" >  $GEN_DIR/update-credentials-folder-level.groovy
-curl --data-urlencode "script=$(cat $GEN_DIR/update-credentials-folder-level.groovy)" \
+cat ./credentials-migration/update-credentials-folder-level.groovy | sed  "s#^\/\/ encoded.*#encoded = [new File(\"/var\/jenkins_home\/folder-imports.txt\").text]#g" >  $GENDIR/update-credentials-folder-level.groovy
+curl --data-urlencode "script=$(cat $GENDIR/update-credentials-folder-level.groovy)" \
 --user $TOKEN ${BASE_URL}/${CONTROLLER_NAME}/scriptText
 
 # EXPORT SYSTEM CREDENTIALS
 echo "------------------  EXPORT SYSTEM CREDENTIALS  ------------------"
 curl -o ./credentials-migration/export-credentials-system-level.groovy https://raw.githubusercontent.com/cloudbees/jenkins-scripts/master/credentials-migration/export-credentials-system-level.groovy
 curl --data-urlencode "script=$(cat ./credentials-migration/export-credentials-system-level.groovy)" \
---user $TOKEN ${BASE_URL}/teams-${CONTROLLER_NAME}/scriptText  -o $GEN_DIR/test-system-folder.creds
-tail -n 1  $GEN_DIR/test-system-folder.creds | sed  -e "s#\[\"##g"  -e "s#\"\]##g"  | tee  $GEN_DIR/system-imports.txt
+--user $TOKEN ${BASE_URL}/teams-${CONTROLLER_NAME}/scriptText  -o $GENDIR/test-system-folder.creds
+tail -n 1  $GENDIR/test-system-folder.creds | sed  -e "s#\[\"##g"  -e "s#\"\]##g"  | tee  $GENDIR/system-imports.txt
 
 # IMPORT SYSTEM CREDENTIALS
 echo "-------------------- IMPORT SYSTEM CREDENTIALS  ------------------"
-kubectl cp $GEN_DIR/system-imports.txt ${CONTROLLER_NAME}-0:/var/jenkins_home/
+kubectl cp $GENDIR/system-imports.txt ${CONTROLLER_NAME}-0:/var/jenkins_home/
 curl -o ./credentials-migration/update-credentials-system-level.groovy https://raw.githubusercontent.com/cloudbees/jenkins-scripts/master/credentials-migration/update-credentials-system-level.groovy
-cat ./credentials-migration/update-credentials-system-level.groovy | sed  "s#^\/\/ encoded.*#encoded = [new File(\"/var\/jenkins_home\/system-imports.txt\").text]#g" >  $GEN_DIR/update-credentials-folder-level.groovy
-curl --data-urlencode "script=$(cat $GEN_DIR/update-credentials-folder-level.groovy)" \
+cat ./credentials-migration/update-credentials-system-level.groovy | sed  "s#^\/\/ encoded.*#encoded = [new File(\"/var\/jenkins_home\/system-imports.txt\").text]#g" >  $GENDIR/update-credentials-folder-level.groovy
+curl --data-urlencode "script=$(cat $GENDIR/update-credentials-folder-level.groovy)" \
 --user $TOKEN ${BASE_URL}/${CONTROLLER_NAME}/scriptText
